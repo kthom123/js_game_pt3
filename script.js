3b4ee955a4ca31c2dd77610aecfbd1d1e3984ef1
Kyle Thomas <kylejwthomas@gmail.com>
2022-06-08 12:11:30 -0900

    changed to enemy 2

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ let gameFrame = 0;
 class Enemy {
     constructor(){
         this.image = new Image();
-        this.image.src = './enemies/enemy1.png';
-        // this.speed = Math.random() * 4 - 2;
-        this.spriteWidth = 293;
-        this.spriteHeight = 155;
+        this.image.src = './enemies/enemy2.png';
+        this.speed = Math.random() * 4 + 1;
+        this.spriteWidth = 266;
+        this.spriteHeight = 188;
         this.width = this.spriteWidth / 2.5;
         this.height = this.spriteHeight / 2.5;
         this.x = Math.random() * (canvas.width - this.width);
@@ -23,8 +23,9 @@ class Enemy {
         this.flapSpeed = Math.floor(Math.random() * 3 + 1);
     }
     update(){
-        this.x += Math.random() * 5 - 2.5;
-        this.y += Math.random() * 5 - 2.5;
+        this.x -= this.speed;
+        // this.y += Math.random() * 5 - 2.5;
+        if (this.x + this.width < 0) this.x = canvas.width;
         // animate sprites
         if (gameFrame % this.flapSpeed === 0){
           this.frame > 4 ? this.frame = 0 : this.frame++;
