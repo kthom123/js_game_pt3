a130b9947cdbcf08179f9836d98f5845b225b9c1
Kyle Thomas <kylejwthomas@gmail.com>
2022-08-05 14:02:43 +0900

    setup of js

---
+++ script.js
@@ -0,0 +1,25 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById('canvas3');
+const ctx = canvas.getContext('2d');
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
+/*
+enemy1 = {
+    x: 10,
+    y: 50,
+    width: 100,
+    height: 100,
+}*/
+
+class Enemy {
+
+}
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemy1.x++;
+    enemy1.y++;
+    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+    requestAnimationFrame(animate);
+}
+animate();
